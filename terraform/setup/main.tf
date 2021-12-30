48de97c5df473427aea7d7e58eabdc84b284d394
Jason Yee <446031+jwsy@users.noreply.github.com>
2021-12-29 20:20:29 -0500

    Update docs (#1)
    * Update docs and examples of how to specify tf vars
    
    * Fix typos in quickstart
    
    * Update docs and quickstart
    
    * Specify project for TF for use in GCP Console
    
    * Add auto-approve to quickstart
    
    Co-authored-by: Jason Yee <jyee@techintsolutions.com>

--- terraform/setup/main.tf
+++ terraform/setup/main.tf
@@ -1,6 +1,21 @@
+# Example run
+# terraform plan -var "gcp_billing_account_name=My Billing Account FY22" -var 'project_id=cvah-helk-training-fy22' 
+# terraform apply -auto-approve -var "gcp_billing_account_name=My Billing Account FY22" -var 'project_id=cvah-helk-training-fy22' 
+
 variable "project_id" {
     type            = string
     default         = "cvah-helk-training"
+    # Example: 
+    # terraform plan -var 'project_id=cvah-helk-training-fy22' 
+    # The display name of the project created will be "Terraform HELK Training"
+}
+
+variable "gcp_billing_account_name" {
+    type            = string
+    default         = "My Billing Account"
+    description     = "The billing account name from billing https://console.cloud.google.com/billing."
+    # Example:
+    # terraform plan -var "gcp_billing_account_name=My Billing Account FY22"
 }
 
 provider "google" {
@@ -9,16 +24,18 @@ provider "google" {
 }
 
 data "google_billing_account" "acct" {
-  display_name = "My Billing Account"
+  display_name = var.gcp_billing_account_name
   open         = true
 }
 
 resource "google_project_service" "service_api" {
-   service                      = "serviceusage.googleapis.com"
-   disable_on_destroy           = false
+    project                     = var.project_id
+    service                     = "serviceusage.googleapis.com"
+    disable_on_destroy          = false
 }
 
 resource "google_project_service" "compute_api" {
+    project                     = var.project_id
     depends_on 			        = [google_project_service.service_api]
     service 			        = "compute.googleapis.com"
     disable_on_destroy          = false
